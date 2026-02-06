ded6b2c40ed55858e69e2ce9a36c0efb76d054ae
nguyenkhaan <nguyenkhaan2006@gmail.com>
2026-02-06 12:30:17 +0700

    fix(prisma): change the migrations file in schema

--- prisma/migrations/20260206051724_init/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-CREATE SEQUENCE userrole_id_seq;
-ALTER TABLE "UserRole" ALTER COLUMN "id" SET DEFAULT nextval('userrole_id_seq');
-ALTER SEQUENCE userrole_id_seq OWNED BY "UserRole"."id";
